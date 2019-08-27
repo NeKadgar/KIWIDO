b357d6903123a0bb708dc0f7ba3ec99dfc584cbc
NeKadgar <37880360+NeKadgar@users.noreply.github.com>
2019-08-27 13:33:21 +0300

    Update settings.py

--- server2/settings.py
+++ server2/settings.py
@@ -136,6 +136,9 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
